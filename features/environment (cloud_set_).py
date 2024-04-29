fbaf975aa1f749f4650f3118d9b3a89f8a6f5670
Ilya Dyakov <dyak.ilya@gmail.com>
2024-04-28 21:04:27 -0700

    brows_stack_v2

--- features/environment (cloud_set_).py
+++ features/environment (cloud_set_).py
@@ -5,49 +5,49 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.options import Options
 from app.application import Application
 
+
 def browser_init(context, scenario_name):
     """
     :param scenario_name:
     :param context: Behave context
     """
-    # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Chrome(service=service)
-    #
-    # driver_path = GeckoDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Firefox(service=service)
-    #
-    # ## BROWSERS WITH DRIVERS: provide path to the driver file ###
-    # service = Service(executable_path='C:\Users\User\PycharmProjects\BDD_switch_win')
-    # context.driver = webdriver.Firefox(service=service)
-    #
-    # ## SAFARI ###
-    # context.driver = webdriver.Safari()
-    #
-    # ## HEADLESS MODE ####
-    # options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
-    # service = Service(ChromeDriverManager().install())
-    # context.driver = webdriver.Chrome(
-    #     options=options,
-    #     service=service
-    # )
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
+
+    driver_path = GeckoDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Firefox(service=service)
+
+    ## BROWSERS WITH DRIVERS: provide path to the driver file ###
+    service = Service(executable_path='C:\Users\User\PycharmProjects\BDD_switch_win')
+    context.driver = webdriver.Firefox(service=service)
+
+    ## SAFARI ###
+    context.driver = webdriver.Safari()
+
+    ## HEADLESS MODE ####
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    service = Service(ChromeDriverManager().install())
+    context.driver = webdriver.Chrome(
+        options=options,
+        service=service
+    )
 
     ## BROWSERSTACK ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    bs_user = '****'
-    bs_key = '****'
+    bs_user = 'idyakov_etLzbT'
+    bs_key = 'djMq7KBSFzzFZwu4B3eu'
     url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
 
     options = Options()
     bstack_options = {
-        'os': 'Windows',
-        'osVersion': '10',
-        'browserName': 'Chrome',
-        'sessionName': 'User is able to open Privacy Policy'
+        'osVersion': '13.0',
+        'deviceName': 'Samsung Galaxy S23',
+        'browserName': 'chrome',
+        'sessionName': scenario_name
     }
-
     options.set_capability('bstack:options', bstack_options)
     context.driver = webdriver.Remote(command_executor=url, options=options)
 
@@ -75,4 +75,6 @@ def after_scenario(context, feature):
     context.driver.quit()
 
 
-#### BrowserStack ####
+#### BrowserStack #####
+
+
